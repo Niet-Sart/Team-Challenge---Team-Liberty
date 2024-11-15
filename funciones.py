fa9cae7da1a1f0a8358b5cb573df0e2a702f37ae
jesusgnzalz <jesusgb555@gmail.com>
2024-11-15 18:06:32 +0100

    Update funciones.py
    Gestión de los intentos de disparo en una posición ya disparada.
    
    La idea es que si el jugador ya ha disparado en esa coordenada, se le avise para que elija otra posición

--- funciones.py
+++ funciones.py
@@ -20,3 +20,25 @@ def maquina_dispara(tablero):
     coordenada = x,y
     posiciones_disparadas.append(coordenada)
     return disparar(tablero, coordenada)
+
+# JESUS
+
+# Gestión de los intentos de disparo en una posición ya disparada
+def disparar(tablero, coordenada):
+    x, y = coordenada
+    if x < 0 or x >= TAMANO or y < 0 or y >= TAMANO:
+        print("Coordenada fuera de rango")
+        return tablero
+
+    if coordenada in posiciones_disparadas:
+        print("Ya has disparado en esta posición. Elige otra coordenada.")
+        return tablero  # Devuelve el tablero sin cambios, para poder realizar más intentos
+
+    posiciones_disparadas.append(coordenada)
+    if tablero[x, y] == celda_agua:
+        print("Agua!")
+    elif tablero[x, y] == celda_disparo:
+        print("Tocado!")
+        tablero[x, y] = celda_disparo  # Actualiza el tablero con el disparo
+    return tablero
+ # La idea es que si el jugador ya ha disparado en esa coordenada, se le avise para que elija otra posición
